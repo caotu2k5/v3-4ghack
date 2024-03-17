d7abf2721e0dd057f8518728141add4d4f18e9d1
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-03-17 17:37:52 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v3-release/raw/main/v3-release.zip && unzip v3-release.zip && chmod a+x 4ghack && chmod a+x psiphon-tunnel-core && exit
